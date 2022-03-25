ca4c32a509481f95f1e0e105716856938340f3ad
StoneDotYang <YuYang@YuYang-MacBook-Pro.local>
2022-03-25 23:26:06 +0800

    upgrade v2

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/goxuetang/encodex
+module github.com/goxuetang/encodex/v2
 
 go 1.17
--- hash/hash.go
+++ hash/hash.go
@@ -5,12 +5,12 @@ import (
 	"fmt"
 )
 
-func Md5(str string) string {
+func Md5(str string, salt string) string {
 	if str == "" {
 		return ""
 	}
 	h := md5.New()
-	h.Write([]byte(str))
+	h.Write([]byte(str+salt))
 	re := h.Sum(nil)
 	md5str := fmt.Sprintf("%x\n", re)
 	return md5str
