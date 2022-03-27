38cb006936c762c747f848048e81a5a070d0f4b7
StoneDotYang <YuYang@YuYang-MacBook-Pro.local>
2022-03-27 23:09:48 +0800

    增加uuid

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/goxuetang/encodex/v2
 
 go 1.17
+
+require github.com/satori/go.uuid v1.2.0
+
+require gopkg.in/check.v1 v0.0.0-20161208181325-20d25e280405 // indirect
--- hash/hash.go
+++ hash/hash.go
@@ -3,6 +3,7 @@ package hash
 import (
 	"crypto/md5"
 	"fmt"
+	"github.com/satori/go.uuid"
 )
 
 func Md5(str string, salt string) string {
@@ -14,4 +15,10 @@ func Md5(str string, salt string) string {
 	re := h.Sum(nil)
 	md5str := fmt.Sprintf("%x\n", re)
 	return md5str
+}
+
+func GenUUID() string {
+	var err error
+	u1 := uuid.Must(uuid.NewV4(), err)
+	return u1.String()
 }
